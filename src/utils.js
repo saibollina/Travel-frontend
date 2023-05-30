d4e4fc2380eb4e58e58038a1e24169f1ff19e070
saibollina <s.bollina@eagles.oc.edu>
2023-05-30 15:55:50 -0500

    images changes

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-
-
-export const getImageUrl = (path) => {
-    return "/images/"+path
+export const getImageUrl = (path) => { 
+    return "http://ec2-3-137-205-68.us-east-2.compute.amazonaws.com/travel-frontend/images/"+path 
 }
