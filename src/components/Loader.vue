6df894b941dbf2c23d2a4df7ef57353d3a1a268f
saibollina <127999605+saibollina@users.noreply.github.com>
2023-05-29 23:09:19 -0500

    loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="d-flex align-items-center justify-content-center" style="height: 100vh;">
+      <div class="spinner-grow text-dark" role="status" style="margin-bottom:400px;">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+
+</template>
